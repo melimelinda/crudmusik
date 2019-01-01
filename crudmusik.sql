3701ff14a3ccebc6e6585bcef9ac5a23a9b18305
Meli Melinda <melinawada29@gmail.com>
2019-01-01 20:54:20 +0700

    ref6

--- crudmusik.sql
+++ crudmusik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2018 at 05:39 AM
+-- Generation Time: Jan 01, 2019 at 02:52 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -23,37 +23,125 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `musik`
+-- Table structure for table `migrations`
 --
 
-CREATE TABLE `musik` (
-  `id_musik` int(11) NOT NULL,
-  `penyanyi` text NOT NULL,
-  `judul` text NOT NULL,
-  `file` varchar(250) NOT NULL,
-  `lirik` text NOT NULL,
-  `genre` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(4, '2014_10_12_000000_create_users_table', 1),
+(5, '2014_10_12_100000_create_password_resets_table', 1),
+(6, '2018_12_28_035529_create_posts_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `penyanyi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `judul` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lirik` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `genre` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `penyanyi`, `judul`, `lirik`, `genre`, `created_at`, `updated_at`) VALUES
+(7, 'Jennie', 'solo', 'nmnxmansjkdascbxcasuihasuichaschascsdfwerwewerewrwerewrewrew', 'pop', '2018-12-31 02:51:26', '2018-12-31 23:14:38'),
+(9, 'Melly Goeslaw', 'Bimbang', 'Pertama kali aku tergugah\r\nDalam setiap kata yang kau ucap\r\nBila malam tlah datang\r\nTerkadang ingin ku tulis semua perasaan\r\n\r\nKata orang rindu itu indah\r\nNamun bagiku ini menyiksa\r\nSejenak ku fikirkan untuk ku benci saja dirimu\r\nNamun sulit ku membenci\r\n\r\nPejamkan mata bila kuingin bernafas lega\r\nDalam anganku aku berada disatu\r\npersimpangan jalan yang sulit kupilih\r\n\r\nKu peluk semua indah hidupku\r\nHikmah yang ku rasa sangat tulus\r\nAda dan tiada cinta bagiku tak mengapa\r\nNamun ada yang hilang separuh\r\nDiriku', 'POP', '2018-12-31 22:53:47', '2018-12-31 22:55:38'),
+(11, 'meli', 'bimbang', 'blalababab', 'pop', '2019-01-01 04:59:39', '2019-01-01 04:59:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `musik`
+-- Indexes for table `migrations`
 --
-ALTER TABLE `musik`
-  ADD PRIMARY KEY (`id_musik`);
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `musik`
+-- AUTO_INCREMENT for table `migrations`
 --
-ALTER TABLE `musik`
-  MODIFY `id_musik` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
